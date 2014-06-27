6ff10c237e9e14b65e2a38e000c70daf9ee94360
Pat Pannuto <pat.pannuto@gmail.com>
2014-06-27 16:08:38 -0400

    Add inital view logic for all routes; all untested

--- chezbetty/__init__.py
+++ chezbetty/__init__.py
@@ -12,9 +12,25 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
 
+    config.add_route('about', '/about.html')
+
+    config.add_route('items', '/items.html')
+    config.add_route('item', '/item/{barcode}')
+
+    config.add_route('users', '/users.html')
+    config.add_route('user', '/user/{umid}')
+
+    config.add_route('purchase', '/purchase.html')
+    config.add_route('purchase_new', '/purchase/{umid}')
+
+    config.add_route('deposit', '/deposit.html')
+    config.add_route('deposit_new', '/deposit/new')
+
+    # Old testing / startup stubs
     config.add_route('home', '/')
     config.add_route('user_json', '/user/{uid}/json')
     config.add_route('item_json', '/item/{iid}/json')
+
     config.scan(".views")
 
     return config.make_wsgi_app()
--- chezbetty/views.py
+++ chezbetty/views.py
@@ -1,9 +1,61 @@
+from pyramid.renderers import render
 from pyramid.response import Response
 from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
 
 from .models import *
+import datalayer
+
+@view_config(route_name='about', renderer='templates/about.jinja2')
+def about(request):
+    return {}
+
+@view_config(route_name='purchase', renderer='templates/purchase.jinja2')
+def purchase(request):
+    return {}
+
+@view_config(route_name='purchase_new', request_method='POST', renderer='templates/purchase_complete.jinja2')
+def purchase_new(request):
+    user = User.from_umid(request.matchdict['umid'])
+    transaction = datalayer.purchase(user, request.POST.items())
+    return {'transaction': transaction}
+
+@view_config(route_name='items', renderer='templates/items.jinja2')
+def items(request):
+    items = DBSession.query(Item).all()
+    return {'items': items}
+
+@view_config(route_name='item', renderer='json')
+def item(request):
+    item = Item.from_barcode(request.matchdict['barcode'])
+    item_html = render('templates/item.jinja2', {'item': item})
+    return {'item_html' : item_html}
+
+@view_config(route_name='users', renderer='templates/users.jinja2')
+def users(request):
+    users = DBSession.query(User).all()
+    return {'users': users}
+
+@view_config(route_name='user', renderer='templates/user.jinja2')
+def user(request):
+    user = User.from_umid(request.matchdict['umid'])
+    return {'user': user}
+
+@view_config(route_name='deposit', renderer='templates/deposit.jinja2')
+def deposit(request):
+    return {}
+
+@view_config(route_name='deposit_new', request_method='POST', renderer='templates/deposit_new.jinja2')
+def deposit_new(request):
+    user = User.from_umid(request.POST['umid'])
+    amount = float(request.POST['amount'])
+    transaction = datalayer.deposit(user, amount)
+    return {'transaction': transaction}
+
+
+#######################
+## Old testing / startup stubs
 
 @view_config(route_name='home', renderer='templates/home.jinja2')
 def my_view(request):
