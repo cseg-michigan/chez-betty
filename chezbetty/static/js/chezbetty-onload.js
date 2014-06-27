85c098777d16398ea3ec1855f90486f9168ecfc5
Brad Campbell <bradjc5@gmail.com>
2014-06-27 13:30:49 -0400

    barcode scanner to ajax json req

--- chezbetty/static/js/chezbetty-onload.js
+++ chezbetty/static/js/chezbetty-onload.js
@@ -13,44 +13,13 @@ $("#purchase_table tbody").on('click', '.btn-remove-item', function () {
 	calculate_total();
 });
 
-$("#btn-add-item").click(function () {
-	items = [{"name": "Coke",
-	        "price": 1.33,
-	        "id": 2739821,
-	        "item_html": '<tr id="purchase-item-2739821" class="purchase-item"><td class="item-actions"><button type="button" class="btn btn-danger btn-remove-item">X</button></td>   <td class="item-title">Coke</td>  <td class="item-quantity">1</td>  <td class="item-price">$1.33</td>  <td class="hidden item-price-single">1.33</td></tr>'},
-
-           {"name": "Coffee",
-	        "price": 5.43,
-	        "id": 88,
-	        "item_html": '<tr id="purchase-item-88" class="purchase-item">  <td class="item-actions"><button type="button" class="btn btn-danger btn-remove-item">X</button></td>  <td class="item-title">Coffee</td>  <td class="item-quantity">1</td>  <td class="item-price">$5.43</td>  <td class="hidden item-price-single">5.43</td></tr>'},
-
-           {"name": "Peach",
-	        "price": 0.74,
-	        "id": 1234,
-	        "item_html": '<tr id="purchase-item-1234" class="purchase-item">  <td class="item-actions"><button type="button" class="btn btn-danger btn-remove-item">X</button></td>   <td class="item-title">Peach</td>  <td class="item-quantity">1</td>  <td class="item-price">$0.74</td>  <td class="hidden item-price-single">0.74</td></tr>'}];
-	
-	item = items[Math.floor((Math.random()*items.length))];
-
-	// First, if this is the first item hide the empty order row
-	$("#purchase-empty").hide();
-
-	// Check if this item is already present. In that case we only
-	// need to increment the quantity and price
-	if ($("#purchase-item-" + item.id).length) {
-		item_row = $("#purchase-item-" + item.id);
-
-		// Increment the quantity
-		quantity = parseInt(item_row.children(".item-quantity").text()) + 1;
-		item_price = parseFloat(item_row.children(".item-price-single").text());
-
-		item_row.children(".item-quantity").text(quantity);
-		item_row.children(".item-price").text(format_price(quantity*item_price));
-
-	} else {
-		// Add a new item
-		$("#purchase_table tbody").append(item.item_html);
+barcode = "";
+$(document).keypress(function (e) {
+	if (e.which == 13) {
+		// Got new scan!
+		console.log("barcode: " + barcode);
+		add_item(barcode);
+		barcode = "";
 	}
-
-	calculate_total();
-
+	barcode += String.fromCharCode(e.which);
 });
--- chezbetty/static/js/chezbetty.js
+++ chezbetty/static/js/chezbetty.js
@@ -8,6 +8,33 @@ function strip_price (price_str) {
 	return price_str.replace(/\$/g, '');
 }
 
+function add_item (item_id) {
+	$.getJSON("/item/"+item_id+"/json", function (data) {
+
+		// First, if this is the first item hide the empty order row
+		$("#purchase-empty").hide();
+
+		// Check if this item is already present. In that case we only
+		// need to increment the quantity and price
+		if ($("#purchase-item-" + data.id).length) {
+			item_row = $("#purchase-item-" + data.id);
+
+			// Increment the quantity
+			quantity = parseInt(item_row.children(".item-quantity").text()) + 1;
+			item_price = parseFloat(item_row.children(".item-price-single").text());
+
+			item_row.children(".item-quantity").text(quantity);
+			item_row.children(".item-price").text(format_price(quantity*item_price));
+
+		} else {
+			// Add a new item
+			$("#purchase_table tbody").append(data.item_html);
+		}
+
+		calculate_total();
+	});
+}
+
 function calculate_total () {
 	total = 0;
 	$("#purchase_table tbody tr:visible").each(function (index) {
@@ -16,6 +43,7 @@ function calculate_total () {
 			total += line_total;
 		}
 	});
-	
+
 	$("#purchase-total").text(format_price(total));
 }
+
