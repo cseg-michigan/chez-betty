6fad78bb3ea826bd1597ce81b3a84bdce01abf9a
Zakir Durumeric <zakird@gmail.com>
2014-06-27 15:55:04 -0400

    semi-complete cash transaction

--- chezbetty/models/cashtransaction.py
+++ chezbetty/models/cashtransaction.py
@@ -4,14 +4,13 @@ class CashAccount(Base):
     __tablename__ = "cash_accounts"
     
     id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=False, unique=True)
     balance = Column(Float, nullable=False)
     
     def __init__(self, name):
         self.name = name
         self.balance = 0.0
 
-# singletons that 
 
 def make_cash_account(name):
     t = DBSession.query(Account).filter(Account.name == "cashbox").first()
@@ -21,10 +20,6 @@ def make_cash_account(name):
     DBSession.add(t)
     return t
 
-c_cashbox = make_cash_account("cashbox")
-c_chezbetty = make_cash_account("chezbetty")
-c_store = make_cash_account("store")
-c_lost = make_cash_account("lost")
 
 class CashTransaction(Base):
     __tablename__ = 'cash_transactions'
@@ -34,7 +29,22 @@ class CashTransaction(Base):
     timestamp = Column(DateTime, nullable=False, default=datetime.datetime.now)
     amount = Column(Float, nullable=False)
 
-    to_account_id = Column(Integer, ForeignKey("accounts.id"), nullable=False)
-    from_account_id = Column(Integer, ForeignKey("accounts.id"), nullable=False)
+    to_account_id = Column(Integer, ForeignKey("cash_accounts.id"), nullable=False)
+    from_account_id = Column(Integer, ForeignKey("cash_accounts.id"), nullable=False)
     
-
+    to_account = relationship(CashAccount, 
+        foreign_keys=[to_account_id,],
+        backref="transactions_to"
+    )
+    
+    from_account = relationship(CashAccount, 
+        foreign_keys=[from_account_id,],
+        backref="transactions_from"
+    )
+    
+    def __str__(self):
+        return "<CashTransaction (%i: %s -> %s: $%f)>" % (
+                self.id,
+                self.from_account_id.name,
+                self.to_account.name,
+                self.amount)
