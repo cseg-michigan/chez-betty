f8529942776123307562d09cfbea7d4d7619c277
Zakir Durumeric <zakird@gmail.com>
2014-06-27 11:32:15 -0400

    adding user model

--- chezbetty/models/model.py
+++ chezbetty/models/model.py
@@ -2,6 +2,8 @@ from sqlalchemy import (
     Column,
     Index,
     Integer,
+    String
+    Decimal
     Text,
     )
 
@@ -16,12 +18,3 @@ from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
-
-
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
-
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
---
+++ chezbetty/models/user.py
@@ -0,0 +1,52 @@
+from model import *
+
+class LDAPUserNotFoundException(Exception):
+    pass
+
+class LDAPLookup(object):
+    """class that allows lookup of an individual in the UM directory
+    based on Michigan ID number, uniqname, MCARD"""
+
+    USERNAME = "cn=CSEG-McDirApp001,ou=Applications,o=services"
+    PASSWORD = ""
+
+    def __init__(self):
+        pass
+
+    def lookup_umid(self, umid):
+        pass
+
+    def lookup_uniqname(self, uniqname):
+        pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    uniqname = Column(String(8), nullable=False, unique=True)
+    umid = Column(String(8), nullable=False, unique=True)
+    display_name = Column(String(255), nullable=False)
+    disabled = Column(Boolean, nullable=False, default=False)
+    role = Column(Enum("user", "manager", "administrator", name="user_type"),
+            nullable=False, default="user")
+    created_at = Column(DateTime, default=datetime.datetime.now)
+
+    @classmethod
+    def from_uniqname(cls, uniqname):
+        pass
+
+    @classmethod
+    def from_umid(cls, uniqname):
+        pass
+
+    @classmethod
+    def from_idcard(cls, idcard):
+        pass
+
+    def __init__(self, uniqname, umid, display_name):
+        self.uniqname = uniqname
+        self.umid = umid
+        self.display_name = display_name
+
+
