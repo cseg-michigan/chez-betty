d01a6e5279862a12879cf1e895bd16c2533daffb
Zakir Durumeric <zakird@gmail.com>
2014-06-28 00:36:29 -0400

    cash transaction allow null user

--- chezbetty/models/cashtransaction.py
+++ chezbetty/models/cashtransaction.py
@@ -63,5 +63,5 @@ class CashTransaction(Base):
 class CashDeposit(CashTransaction):
     def __init__(self, amount, transaction):
         c_cashbox = make_cash_account("cashbox")
-        CashTransaction.__init__(None, c_cashbox, amount, transaction)
+        CashTransaction.__init__(None, c_cashbox, amount, transaction, None)
         
