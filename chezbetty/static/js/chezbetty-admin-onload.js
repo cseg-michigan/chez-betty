8ed8694c51d5c2ac385cce3bbb85afc1e40c3e45
Brad Campbell <bradjc5@gmail.com>
2014-06-29 12:20:01 -0400

    add_item js

--- chezbetty/static/js/chezbetty-admin-onload.js
+++ chezbetty/static/js/chezbetty-admin-onload.js
@@ -35,4 +35,34 @@ $(".edit-item-row").on("click", "button", function () {
 	}
 });
 
-//$("#btn-items-add-row")
+// Add a new row to the add items form
+$("#btn-items-add-row").click(function () {
+
+	// Instead of counting each time, just keep the number of lines around
+	// in a hidden element.
+	var item_lines_count = parseInt($("#new-items-number").val());
+
+	// Copy row 0 to create a new row
+	container = $("#new-item-0").clone().attr("id", "new-item-"+item_lines_count);
+	container.find("input").each(function (index) {
+		// Update the ID to the next number
+		id = $(this).attr("id");
+		name_pieces = id.split("-");
+		name_pieces[name_pieces.length-1] = item_lines_count;
+		new_id = name_pieces.join("-");
+		$(this).attr("id", new_id);
+		$(this).attr("name", new_id);
+		// Clear the value if there is text in the first row already
+		$(this).val("");
+		// Reset the checkmark so new products are enabled by default
+		if ($(this).is(":checkbox")) {
+			$(this).prop("checked", "checked");
+		}
+	});
+
+	// Add the new row to the page
+	$("#new-items").append(container);
+
+	// Update the number of new items to be added
+	$("#new-items-number").val(item_lines_count+1);
+});
