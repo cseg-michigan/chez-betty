c0275d7d3754512c75463f7eb7bfe287cccdceae
Zakir Durumeric <zakird@gmail.com>
2014-06-30 00:21:35 -0400

    enums need names

--- chezbetty/models/account.py
+++ chezbetty/models/account.py
@@ -4,7 +4,7 @@ class Account(Versioned, Base):
     __tablename__ = "accounts"
     
     id = Column(Integer, primary_key=True, nullable=False)
-    type = Column(Enum("user", "placeholder"), nullable=False)
+    type = Column(Enum("user", "placeholder", name="account_type"), nullable=False)
     balance = Column(Float, nullable=False)
     name = Column(String(255), nullable=False)
     created_at = Column(DateTime, default=datetime.datetime.now)
