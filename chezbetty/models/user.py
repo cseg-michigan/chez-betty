9a24d4fbe26e569e16cfd29aeac2fce199f925c8
Zakir Durumeric <zakird@gmail.com>
2014-06-30 13:59:05 -0400

    fucking python bullshit exception handling

--- chezbetty/models/user.py
+++ chezbetty/models/user.py
@@ -43,7 +43,7 @@ class LDAPLookup(object):
                     ldap3.SEARCH_SCOPE_WHOLE_SUBTREE,
                     attributes=self.ATTRIBUTES
             )
-        except LDAPSocketOpenError, e:
+        except ldap3.LDAPSocketOpenError as e:
             # sometimes our connections time out
             self.__conn = None
             self.__connect()
