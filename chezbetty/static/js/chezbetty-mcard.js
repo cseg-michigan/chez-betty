8da5f9d93e1043aae68e5df55794442eef310036
Brad Campbell <bradjc5@gmail.com>
2014-06-30 01:51:55 -0400

    added spinner back

--- chezbetty/static/js/chezbetty-mcard.js
+++ chezbetty/static/js/chezbetty-mcard.js
@@ -28,7 +28,7 @@ $(document).keypress(function (e) {
 			  top: '50%', // Top position relative to parent
 			  left: '50%' // Left position relative to parent
 			};
-			var spinner_location = document.getElementById('login-panel');
+			var spinner_location = document.getElementById('front-buttons');
 			var spinner = new Spinner(opts).spin(spinner_location);
 
 			window.location.replace("/purchase/" + umid);
