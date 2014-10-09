166593242b85bd9b8dda0b7379a9f0aa63d2c388
Brad Campbell <bradjc5@gmail.com>
2014-10-09 03:21:58 -0400

    only try loggin in with mcard on terminal
    only if betty is logged in

--- chezbetty/static/js/chezbetty-mcard.js
+++ chezbetty/static/js/chezbetty-mcard.js
@@ -23,46 +23,49 @@ function clear_spinner () {
 
 keyboard_input = "";
 input_timer = null;
-$(document).keypress(function (e) {
-	alert_clear();
 
-	if (input_timer) {
-		clearInterval(input_timer);
-	}
-	input_timer = setInterval(submit_input, 500);
+if (is_service_account) {
+	$(document).keypress(function (e) {
+		alert_clear();
 
-	if (keyboard_input.length > 0 && $(".spinner").length) {
+		if (input_timer) {
+			clearInterval(input_timer);
+		}
+		input_timer = setInterval(submit_input, 500);
 
-		$("#index-main").hide();
-		$("#front-buttons").hide();
+		if (keyboard_input.length > 0 && $(".spinner").length) {
 
-		// Display spinner in case loading the user takes a while.
-		var opts = {
-		  lines: 11, // The number of lines to draw
-		  length: 18, // The length of each line
-		  width: 9, // The line thickness
-		  radius: 19, // The radius of the inner circle
-		  corners: 1, // Corner roundness (0..1)
-		  rotate: 0, // The rotation offset
-		  direction: 1, // 1: clockwise, -1: counterclockwise
-		  color: '#000', // #rgb or #rrggbb or array of colors
-		  speed: 1, // Rounds per second
-		  trail: 60, // Afterglow percentage
-		  shadow: false, // Whether to render a shadow
-		  hwaccel: false, // Whether to use hardware acceleration
-		  className: 'spinner', // The CSS class to assign to the spinner
-		  zIndex: 2e9, // The z-index (defaults to 2000000000)
-		  top: '50%', // Top position relative to parent
-		  left: '50%' // Left position relative to parent
-		};
-		var spinner_location = document.getElementById('spinner-box');
-		var spinner = new Spinner(opts).spin(spinner_location);
+			$("#index-main").hide();
+			$("#front-buttons").hide();
 
-		setTimeout(clear_spinner, 10*1000);
+			// Display spinner in case loading the user takes a while.
+			var opts = {
+			  lines: 11, // The number of lines to draw
+			  length: 18, // The length of each line
+			  width: 9, // The line thickness
+			  radius: 19, // The radius of the inner circle
+			  corners: 1, // Corner roundness (0..1)
+			  rotate: 0, // The rotation offset
+			  direction: 1, // 1: clockwise, -1: counterclockwise
+			  color: '#000', // #rgb or #rrggbb or array of colors
+			  speed: 1, // Rounds per second
+			  trail: 60, // Afterglow percentage
+			  shadow: false, // Whether to render a shadow
+			  hwaccel: false, // Whether to use hardware acceleration
+			  className: 'spinner', // The CSS class to assign to the spinner
+			  zIndex: 2e9, // The z-index (defaults to 2000000000)
+			  top: '50%', // Top position relative to parent
+			  left: '50%' // Left position relative to parent
+			};
+			var spinner_location = document.getElementById('spinner-box');
+			var spinner = new Spinner(opts).spin(spinner_location);
 
-		keyboard_input += String.fromCharCode(e.which);
+			setTimeout(clear_spinner, 10*1000);
 
-	} else {
-		keyboard_input += String.fromCharCode(e.which);
-	}
-});
+			keyboard_input += String.fromCharCode(e.which);
+
+		} else {
+			keyboard_input += String.fromCharCode(e.which);
+		}
+	});
+}
