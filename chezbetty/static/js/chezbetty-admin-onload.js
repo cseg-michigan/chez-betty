041a8331776195be7d0a7e3925e7dfc1a1a4d22a
Pat Pannuto <pat.pannuto@gmail.com>
2014-10-23 21:53:53 -0400

    Move events ajax to events page

--- chezbetty/static/js/chezbetty-admin-onload.js
+++ chezbetty/static/js/chezbetty-admin-onload.js
@@ -149,40 +149,6 @@ if ($('#tweet').length > 0) {
 	tweet_char_count();
 }
 
-// EVENTS / TRANSACTIONS
-
-var event_loading = false;
-
-function event_load_more_success (data) {
-  $("#events_showing_text").text("Showing rows 1-"+data.count+".");
-  $("#events_showing_count").text(data.count);
-
-  $("#events_table tr:last").after(data.rows);
-  $(".date").each(prettydate);
-
-  event_loading = false;
-}
-
-function event_load_more_fail () {
-  $("#events_showing_text").text("Error loading more rows.");
-}
-
-$(window).scroll(function event_autoload_onscroll () {
-  if (event_loading) return;
-
-  if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-    event_loading = true;
-    $("#events_showing_text").text($("#events_showing_text").text() + " Loading more rows...");
-    $.ajax({
-      type: "POST",
-      url: "/admin/events/load_more",
-      data: { last: $("#events_showing_count").text() },
-      success: event_load_more_success,
-      error: event_load_more_fail
-    });
-  }
-});
-
 
 // ITEMS
 
