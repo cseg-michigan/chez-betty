47edbd11817a1b4cd891c1a32eee96b86f4cf24b
Brad <bradjc5@gmail.com>
2016-12-19 01:47:17 -0500

    Revert "Use non-breaking hyphen for negatives"
    This reverts commit 072bc2b1c5227c1b51c3b65af9bcb5fa854f4b76.
    
    @ppannuto seems unwilling or unable to fix this. Wrapping doesn't seem
    like a worse problem then a bunch of giberish.
    
    Fixes #273

--- chezbetty/jinja2_filters.py
+++ chezbetty/jinja2_filters.py
@@ -16,7 +16,7 @@ def format_currency(value):
 		return value
 	# We stored currency as floats, avoid the -$0.00 case
 	if p <= -0.01:
-		 return '<span class="negative">&#x2011${:,.2f}</span>'.format(p*-1.0)
+		 return '<span class="negative">-${:,.2f}</span>'.format(p*-1.0)
 	else:
 		return '<span class="positive">${:,.2f}</span>'.format(value)
 
