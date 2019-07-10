62771a4ee85b6a63642d48bb832c2dcd916562f2
Pat Pannuto <pat.pannuto@gmail.com>
2019-07-09 18:34:01 -0700

    point local DB update script to new url

--- update_local_db_from_betty.sh
+++ update_local_db_from_betty.sh
@@ -3,9 +3,9 @@
 set -e
 
 echo "drop database betty" | psql
-ssh chezbetty.eecs.umich.edu 'pg_dump chezbetty | gzip > betty_dump.psql.gz'
-scp chezbetty.eecs.umich.edu:betty_dump.psql.gz .
-ssh chezbetty.eecs.umich.edu 'rm betty_dump.psql.gz'
+ssh chezbetty.store 'pg_dump chezbetty | gzip > betty_dump.psql.gz'
+scp chezbetty.store:betty_dump.psql.gz .
+ssh chezbetty.store 'rm betty_dump.psql.gz'
 gunzip betty_dump.psql.gz
 echo 'create database betty' | psql
 psql betty < betty_dump.psql
